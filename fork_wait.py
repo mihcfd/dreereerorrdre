5186a556820fee348ca86342774b7dacd77a59f2
mihcfd <164729682+mihcfd@users.noreply.github.com>
2024-03-30 19:28:02 +1200

    Update fork_wait.py

--- fork_wait.py
+++ fork_wait.py
@@ -49,6 +49,7 @@ class ForkWait(unittest.TestCase):
 
     def f(self, id):
         while not self.stop:
+            
             self.alive[id] = os.getpid()
             try:
                 time.sleep(SHORTSLEEP)
